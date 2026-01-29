5969105ba8fa8e0f940c79c9032d3bcae1926715
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-01-28 22:06:09 -0600

    Modified GradeService.java

--- event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/GradeService.java
+++ event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/GradeService.java
@@ -1,8 +1,30 @@
 package com.eventdrivenmicroservices.api.service;
 
+import com.eventdrivenmicroservices.api.model.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GradeService {
+
+    private final List<Student> students = new ArrayList<>();
+    private Long nextId = 1L;
+
+    private final EventPublisher eventPublisher;
+
+    public GradeService(EventPublisher eventPublisher){
+        this.eventPublisher = eventPublisher;
+
+    }
+
+    public Student submitGrade(String name, String subject, int score){
+
+        eventPublisher.publsihEvent(event);
+
+        return Student;
+
+    }
 
 }
