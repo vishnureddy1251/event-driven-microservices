f856ef5c6b7f951c050e15bed2f8ace2293972b6
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-01-29 11:15:11 -0600

    Modified GradeService.java

--- event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/GradeService.java
+++ event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/GradeService.java
@@ -1,5 +1,6 @@
 package com.eventdrivenmicroservices.api.service;
 
+import ch.qos.logback.core.CoreConstants;
 import com.eventdrivenmicroservices.api.model.Student;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +22,29 @@ public class GradeService {
 
     public Student submitGrade(String name, String subject, int score){
 
-        eventPublisher.publsihEvent(event);
+        System.out.println("\n SUBMITTING GRADE");
+        System.out.println("_____________");
+        System.out.println(" Student:" + name);
+        System.out.println(" Subject:" + subject);
+        System.out.println("Score" + score);
 
-        return Student;
+        Student student = new Student(nextId++, name, subject, score);
+        students.add(student);
+
+        System.out.println("Grade:" + student.getGrade());
+        System.out.println("Student record created!");
+
+        GradeSubmittedEvent event = new GradeSubmittedEvent(
+                student.getName(),
+                student.getSubject(),
+                student.getScore(),
+                student.getGrade(),
+                getCurrentTimestamp()
+        );
+
+        eventPublisher.publishEvent(event);
+
+        return student;
 
     }
 
