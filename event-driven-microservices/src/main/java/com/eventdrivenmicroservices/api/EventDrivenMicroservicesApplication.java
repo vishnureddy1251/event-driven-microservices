58023600d0b1be273d030df1c58b28e664b5428b
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-01 20:35:07 -0600

    Modified EventDrivenMicroservicesApplication.java

--- event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/EventDrivenMicroservicesApplication.java
+++ event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/EventDrivenMicroservicesApplication.java
@@ -10,19 +10,18 @@ public class EventDrivenMicroservicesApplication {
 
 		System.out.println("========================================");
 		System.out.println("Starting Event-Driven Microservices");
-		System.out.println("Learning Project");
 		System.out.println("========================================\n");
 
 		SpringApplication.run(EventDrivenMicroservicesApplication.class, args);
 
 		System.out.println("\n Application started successfully!");
-		System.out.println(" API: http://localhost:8080/api/grades");
-		System.out.println(" Endpoints:");
-		System.out.println("   GET  http://localhost:8080/api/grades");
-		System.out.println("   POST http://localhost:8080/api/grades/submit");
-		System.out.println("   GET  http://localhost:8080/api/grades/students");
-		System.out.println("   POST http://localhost:8080/api/grades/process-all");
-		System.out.println("========================================\n");
+		System.out.println("	 API: http://localhost:8080/api/grades");
+		System.out.println(" 	 Endpoints:");
+		System.out.println("   		GET  http://localhost:8080/api/grades");
+		System.out.println("   		POST http://localhost:8080/api/grades/submit");
+		System.out.println("   		GET  http://localhost:8080/api/grades/students");
+		System.out.println("   		POST http://localhost:8080/api/grades/process-all");
+		System.out.println("=====================================================================\n");
 	}
 
 }
