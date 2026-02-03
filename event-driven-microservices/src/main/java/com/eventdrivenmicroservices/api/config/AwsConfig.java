5ce153174abcf4ce092a018eb3c0520455d3e5aa
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-02 22:48:10 -0600

    Modified AwsConfig.java

--- event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/config/AwsConfig.java
+++ event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/config/AwsConfig.java
@@ -1,7 +1,14 @@
 package com.eventdrivenmicroservices.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+import java.net.URI;
 
 @Configuration
 public class AwsConfig {
@@ -17,4 +24,17 @@ public class AwsConfig {
 
     @Value("${aws.secret-access-key}")
     private String secretAccessKey;
+
+    @Bean
+    public SqsClient sqsClient(){
+        return SqsClient.builder()
+                .endpointOverride(URI.create(awsEndpoint))
+                .region(Region.of(awsRegion))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                        )
+                )
+                .build();
+    }
 }
