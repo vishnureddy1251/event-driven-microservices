c77e31d615bea08904310254d7fc8e5f950d8789
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-03 22:17:41 -0600

    Modified SqsEventProcessor.java

--- event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/SqsEventProcessor.java
+++ event-driven-microservices/src/main/java/com/eventdrivenmicroservices/api/service/SqsEventProcessor.java
@@ -1,6 +1,7 @@
 package com.eventdrivenmicroservices.api.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
@@ -10,4 +11,13 @@ public class SqsEventProcessor {
     private final SqsClient sqsClient;
     private final EventProcessor eventProcessor;
     private final ObjectMapper objectMapper;
+
+    @Value("${aws.sqs.queue-url}")
+    private String queueUrl;
+
+    public SqsEventProcessor(SqsClient sqsClient, EventProcessor eventProcessor) {
+        this.sqsClient = sqsClient;
+        this.eventProcessor = eventProcessor;
+        this.objectMapper = new ObjectMapper();
+    }
 }
